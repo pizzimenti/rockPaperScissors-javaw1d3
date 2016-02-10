e369db5914d507b7000e57eee386c28a0be79d9a
Polina Nenchev and tyler suderman <tcsuder@gmail.com>
2016-02-10 11:58:59 -0800

    midrefactor

--- src/main/java/RockPaperScissors.java
+++ src/main/java/RockPaperScissors.java
@@ -5,17 +5,47 @@ import spark.ModelAndView;
 import spark.template.velocity.VelocityTemplateEngine;
 import static spark.Spark.*;
 
-public class RockPaperScissors {
-  public static void main( String[] args ) {}
-
-  public static boolean checkWinner(String play1,String play2) {
-    if (play1.equalsIgnoreCase("rock") && play2.equalsIgnoreCase("scissors")){
-      return true;
-    } else if (play1.equalsIgnoreCase("paper") && play2.equalsIgnoreCase("scissors")){
-      return true;
-    } else if (play1.equalsIgnoreCase("rock") && play2.equalsIgnoreCase("paper")) {
-      return true;
+  public static String checkWinner(String play1,String play2) {
+    HashMap<String, String> winScenerios = new HashMap<String, String>();
+    String[] combos = {"rock", "scissors", "paper", "rock", "paper", "scissors", "rock", "paper", "scissors"};
+    for (int i = 0; i < 9; i++) {
+      String playOne = combos[i];
+      String playTwo = combos[i+1];
+      String tie = playOne.concat(playOne);
+      String combo = playOne.concat(playTwo);
+      if (i < 3) {
+        winScenerios.put(combo,"player one wins");
+      } else if (i < 7) {
+        winScenerios.put(combo,"player two wins");
+      } else {
+        winScenerios.put(tie,"players tie");
+      }
     }
-      return false;
+    play1 = play1.toLowerCase();
+    play2 = play2.toLowerCase();
+    String userCombo = play1.concat(play2);
+    return "true";
   }
+// public class RockPaperScissors {
+//   public static void main( String[] args ) {}
+//
+//   public static String checkWinner(String play1,String play2) {
+//     HashMap<String, String> winScenerios = new HashMap<String, String>();
+//     String[] combos = {"rock", "scissors", "paper", "rock", "paper", "scissors", "rock"};
+//     for (int i = 0; i < 6; i++) {
+//       String playOne = combos[i];
+//       String playTwo = combos[i+1];
+//       String combo = playOne.concat(playTwo);
+//       if (i < 3) {
+//         winScenerios.put(combo,"player one wins");
+//       } else {
+//         winScenerios.put(combo,"player two wins");
+//       }
+//     }
+//     play1 = play1.toLowerCase();
+//     play2 = play2.toLowerCase();
+//     String userCombo = play1.concat(play2);
+//     return winScenerios.get(userCombo);
+//     // return "Player one and two threw the same sign";
+//   }
 }
--- src/test/java/RockPaperScissorsTest.java
+++ src/test/java/RockPaperScissorsTest.java
@@ -3,21 +3,33 @@ import static org.junit.Assert.*;
 
 public class RockPaperScissorsTest {
   @Test
-  public void checkWinner_rockBeatsScissors_true() {
+  public void checkWinner_rockBeatsScissors_string() {
     RockPaperScissors testRockPaperScissors = new RockPaperScissors();
-    assertEquals(true, testRockPaperScissors.checkWinner("Rock", "Scissors"));
+    assertEquals("player one wins", testRockPaperScissors.checkWinner("Rock", "Scissors"));
   }
 
   @Test
-  public void checkWinner_paperbeatScissors_true() {
+  public void checkWinner_scissorsBeatsPaper_string() {
     RockPaperScissors testRockPaperScissors = new RockPaperScissors();
-    assertEquals(true, testRockPaperScissors.checkWinner("Paper", "Scissors"));
+    assertEquals("player one wins", testRockPaperScissors.checkWinner("Scissors", "Paper"));
   }
 
   @Test
-  public void checkWinner_rockbeatPaper_true() {
+  public void checkWinner_paperBeatsRock_string() {
     RockPaperScissors testRockPaperScissors = new RockPaperScissors();
-    assertEquals(true, testRockPaperScissors.checkWinner("Rock", "Paper"));
+    assertEquals("player one wins", testRockPaperScissors.checkWinner("Paper", "Rock"));
+  }
+
+  @Test
+  public void checkWinner_playerWinCheck_string() {
+    RockPaperScissors testRockPaperScissors = new RockPaperScissors();
+    assertEquals("player two wins", testRockPaperScissors.checkWinner("Scissors", "Rock"));
+  }
+
+  @Test
+  public void checkWinner_tie_string() {
+    RockPaperScissors testRockPaperScissors = new RockPaperScissors();
+    assertEquals("players tie", testRockPaperScissors.checkWinner("Scissors", "Scissors"));
   }
 
 }
